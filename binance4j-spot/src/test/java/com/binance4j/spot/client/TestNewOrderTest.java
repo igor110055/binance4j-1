731c4758b916cdbd5e1d310a04c9498cd26a4837
vivienld <leducvivien@gmail.com>
2022-06-14 02:30:09 +0200

    test: corrected MIN_NOTIONAL FILTER

--- binance4j-spot/src/test/java/com/binance4j/spot/client/TestNewOrderTest.java
+++ binance4j-spot/src/test/java/com/binance4j/spot/client/TestNewOrderTest.java
@@ -1,14 +1,15 @@
 package com.binance4j.spot.client;
 
-import com.binance4j.core.test.ConcurrentTest;
-import com.binance4j.spot.order.NewOrder;
-import com.binance4j.spot.service.TestService;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 import java.math.BigDecimal;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import com.binance4j.core.test.ConcurrentTest;
+import com.binance4j.spot.order.NewOrder;
+import com.binance4j.spot.service.TestService;
 
 class TestNewOrderTest extends ConcurrentTest {
 	final SpotClient client = TestService.CLIENT;
@@ -16,7 +17,7 @@ class TestNewOrderTest extends ConcurrentTest {
 	@Test
 	@DisplayName("Test must not throw an exception")
 	void testNewOrderTest() {
-		NewOrder order = NewOrder.buyMarket("SHIBBUSD", new BigDecimal(1000000));
+		NewOrder order = NewOrder.buyMarket("SHIBBUSD", new BigDecimal(10000000));
 		assertDoesNotThrow(() -> {
 			client.newOrderTest(order).execute();
 		});
